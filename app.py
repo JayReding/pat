340852f4abd20479a56fe5bd450322fe7116d941
JayReding <jay@jayreding.com>
2026-09-08 14:03:56 -0500

    Continue new value analyssis code, refine data structures in database

--- app.py
+++ app.py
@@ -10,6 +10,13 @@ import sqlite3
 #df = pd.read_csv('testdata.csv')
 df_historical = pd.read_sql("SELECT * from test_data WHERE \"Payment Date\" < date('2023-07-17')", sqlite3.connect('pat_test.db'))
 df_preference = pd.read_sql("SELECT * from test_data WHERE \"Payment Date\" >= date('2023-07-17') AND \"Payment Date\" <= date('2023-10-15')", sqlite3.connect('pat_test.db'))
+df_transfers = pd.read_sql("SELECT DISTINCT \"Transfer Number\", \"Transfer Amount\", \"Payment Date\" FROM test_data WHERE \"Payment Date\" >= date('2023-07-17') AND \"Payment Date\" <= date('2023-10-15') ORDER BY \"Payment Date\";", sqlite3.connect('pat_test.db'))
+df_newvalue = pd.read_sql("SELECT * from test_data WHERE \"Unpaid\" IS 1", sqlite3.connect('pat_test.db'))
+
+df_snv = pd.concat([df_transfers, df_newvalue], ignore_index=True)
+
+df_snv["Transaction Date"] = df_snv[["Payment Date", "Invoice Date"]].apply(lambda row: ', '.join(row.dropna()), axis=1)
+df_snv.sort_values(by="Transaction Date", inplace=True)
 
 # Initialize the app
 app = Dash(external_stylesheets=[dbc.themes.ZEPHYR, dbc.icons.FONT_AWESOME] )
@@ -32,61 +39,72 @@ app.layout = html.Div([
     html.H1(children='Preference Analysis Tool'),
      dcc.Tabs([
          dcc.Tab(label='Historical Period', children=[
-                      html.H3(children=f'Historical Period: {history_start.strftime("%m/%d/%Y")} through {history_end.strftime("%m/%d/%Y")}'),
-                        dag.AgGrid(
-                          id="historical",
-                          rowData=df_historical.to_dict('records'),
-                          columnDefs=[
-                              {"field": "Transfer Number"},
-                              {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
-                              {"field": "Payment Date"},
-                              {"field": "Invoice Number"},
-                              {"field": "Invoice Date"},
-                              {"field": "Invoice Due"},
-                              {"field": "Invoice to Payment"},
-                              {"field": "Days Past Due"},
-                          ]
-                      ),
-                      html.Div(children=f'Historical Period Invoice Count: {history_count}'),
-                      html.Div(id="hist-total-output"),
-                      html.Div(id="hist-average_dso"),
-                      html.Div(id="hist-average_dpd"),
-                      html.Div(id="hist-weighted_dso"),
-                      html.Div(id="hist-weighted_dpd"),
-                      html.Div(id="hist-skew")
-        ]),
+            html.H3(children=f'Historical Period: {history_start.strftime("%m/%d/%Y")} through {history_end.strftime("%m/%d/%Y")}'),
+            dag.AgGrid(
+                id="historical",
+                rowData=df_historical.to_dict('records'),
+                columnDefs=[
+                    {"field": "Transfer Number"},
+                    {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
+                    {"field": "Payment Date"},
+                    {"field": "Invoice Number"},
+                    {"field": "Invoice Date"},
+                    {"field": "Invoice Due"},
+                    {"field": "Invoice to Payment"},
+                    {"field": "Days Past Due"},
+                ]
+            ),
+            html.Div(children=f'Historical Period Invoice Count: {history_count}'),
+            html.Div(id="hist-total-output"),
+            html.Div(id="hist-average_dso"),
+            html.Div(id="hist-average_dpd"),
+            html.Div(id="hist-weighted_dso"),
+            html.Div(id="hist-weighted_dpd"),
+            html.Div(id="hist-skew")
+    ]),
 
         dcc.Tab(label='Preference Period', children=[
-                      html.H3(children='Preference Period: 07/17/2023 through 10/15/2023'),
-                        dag.AgGrid(
-                          id="preference",
-                          rowData=df_preference.to_dict('records'),
-                          columnDefs=[
-                              {"field": "Transfer Number"},
-                              {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
-                              {"field": "Payment Date"},
-                              {"field": "Invoice Number"},
-                              {"field": "Invoice Date"},
-                              {"field": "Invoice Due"},
-                              {"field": "Invoice to Payment"},
-                              {"field": "Days Past Due"},
-                          ]
-                      ),
-                      html.Div(children=f'Preference Period Invoice Count: {preference_count}'),
-                      html.Div(id="pref-total-output"),
-                      html.Div(id="pref-average_dso"),
-                      html.Div(id="pref-average_dpd"),
-                      html.Div(id="pref-weighted_dso"),
-                      html.Div(id="pref-weighted_dpd"),
-                      html.Div(id="diff-wavg", style={"font-weight": "bold"})
-        ]),
-        
-        dcc.Tab(label='New Value', children=[
+            html.H3(children='Preference Period: 07/17/2023 through 10/15/2023'),
+            dag.AgGrid(
+              id="preference",
+              rowData=df_preference.to_dict('records'),
+              columnDefs=[
+                {"field": "Transfer Number"},
+                 {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
+                {"field": "Payment Date"},
+                 {"field": "Invoice Number"},
+                 {"field": "Invoice Date"},
+                 {"field": "Invoice Due"},
+                 {"field": "Invoice to Payment"},
+                  {"field": "Days Past Due"},
+            ]
+        ),
+        html.Div(children=f'Preference Period Invoice Count: {preference_count}'),
+        html.Div(id="pref-total-output"),
+        html.Div(id="pref-average_dso"),
+        html.Div(id="pref-average_dpd"),
+        html.Div(id="pref-weighted_dso"),
+        html.Div(id="pref-weighted_dpd"),
+        html.Div(id="diff-wavg", style={"font-weight": "bold"})
+    ]),
+         dcc.Tab(label='New Value', children=[
             html.H3(children='New Value Tab'),
-            html.P(children='This is the content for the New Value tab.')
-
+            html.P(children='This is the content for the New Value tab.'),
+            dag.AgGrid(
+                id="transfers",
+                rowData=df_snv.to_dict('records'),
+                columnDefs=[
+                    {"field": "Transaction Date"},
+                    {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
+                    {"field": "Payment Date"},
+                    {"field": "Invoice Number"},
+                    {"field": "Invoice Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
+                    {"field": "Invoice Date"}
+                ]
+            )
         ])
      ])
+
 ])
  
 @callback(
@@ -139,87 +157,6 @@ def compare_hist_pref():
     pref_weighted_dso = df_preference["Invoice to Payment"].mul(df_preference["Transfer Amount"]).sum() / df_preference["Transfer Amount"].sum()
     diff = (pref_weighted_dso - hist_weighted_dso) / hist_weighted_dso * 100
     return(diff) 
-
-"""app.layout = [
-
-    html.H3(children=f'Historical Period: {history_start.strftime("%m/%d/%Y")} through {history_end.strftime("%m/%d/%Y")}'),
-     dag.AgGrid(
-        id="historical",
-        rowData=df_historical.to_dict('records'),
-        columnDefs=[
-            {"field": "Transfer Number"},
-            {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
-            {"field": "Payment Date"},
-            {"field": "Invoice Number"},
-            {"field": "Invoice Date"},
-            {"field": "Invoice Due"},
-            {"field": "Invoice to Payment"},
-            {"field": "Days Past Due"},
-        ]
-    ),
-    html.Div(children=f'Historical Period Invoice Count: {history_count}'),
-    html.Div(id="hist-total-output"),
-    html.Div(id="hist-average_dso"),
-    html.Div(id="hist-average_dpd"),
-    html.Div(id="hist-weighted_dso"),
-    html.Div(id="hist-weighted_dpd"),
-    html.Div(id="hist-skew"),
-
-    html.H3(children='Preference Period: 07/17/2023 through 10/15/2023'),
-     dag.AgGrid(
-        id="preference",
-        rowData=df_preference.to_dict('records'),
-        columnDefs=[
-            {"field": "Transfer Number"},
-            {"field": "Transfer Amount", "valueFormatter": {"function": "d3.format('($,.2f')(params.value)"}},
-            {"field": "Payment Date"},
-            {"field": "Invoice Number"},
-            {"field": "Invoice Date"},
-            {"field": "Invoice Due"},
-            {"field": "Invoice to Payment"},
-            {"field": "Days Past Due"},
-        ]
-    ),
-     html.Div(children=f'Preference Period Invoice Count: {preference_count}'),
-     html.Div(id="pref-total-output"),
-     html.Div(id="pref-average_dso"),
-     html.Div(id="pref-average_dpd"),
-     html.Div(id="pref-weighted_dso"),
-     html.Div(id="pref-weighted_dpd"),
-     html.Div(id="diff-wavg", style={"font-weight": "bold"})
-]
-
-# Here is where we update our statisticakl analysis for the historical period
-
-@callback(
-    Output("hist-total-output", "children"),
-    Output("hist-average_dso", "children"),
-    Output("hist-average_dpd", "children"),
-    Output("hist-weighted_dso", "children"),
-    Output("hist-weighted_dpd", "children"),
-    Output("hist-skew", "children"),
-    Input("historical", "rowData")
-)
-
-def update_hist_totals(rowData):
-    df = pd.DataFrame(rowData)
-    total = df["Transfer Amount"].sum()
-    average_dso = df["Invoice to Payment"].mean()
-    average_dpd = df["Days Past Due"].mean()
-    weighted_dso = df["Invoice to Payment"].mul(df["Transfer Amount"]).sum() / df["Transfer Amount"].sum()
-    weighted_dpd = df["Days Past Due"].mul(df["Transfer Amount"]).sum() / df["Transfer Amount"].sum()
-    skew = df["Invoice to Payment"].skew()
-    if skew > 1 or skew < -1:
-        skew_warning = " (Warning: Skew is outside the range of -1 to 1, indicating a non-normal distribution)"
-    else:
-        skew_warning = ""
-
-    return f"Historical Period Total Transfer Amount: ${total:,.2f}", f"Historical Period Average DSO: {average_dso:.2f}", f"Historical Period Average DPD: {average_dpd:.2f}", f"Historical Period Weighted DSO: {weighted_dso:.2f}", f"Historical Period Weighted DPD: {weighted_dpd:.2f}", f"Historical Period Skew: {skew:.2f}, {skew_warning}"
-
-
-# Here is where we update our statistical analysis for the preference period
-
-    """
 
 
 # Run the app
